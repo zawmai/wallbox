d954fff2d2bda45075c6ae944dc642146d8fb780
Zaw Mai <10764631+zawmai@users.noreply.github.com>
2021-07-23 00:28:59 -0700

    rename entry js file

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(
-    'index.html', 
-    {root: path.join(__dirname, 'public')}
-  );
+  try {
+    res.sendFile(
+      'index.html', 
+    );
+  } catch (e) {
+    res.send(e);
+    console.error(e);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhoust:${port}`);
+  console.log(path.join(__dirname, 'public'));
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
